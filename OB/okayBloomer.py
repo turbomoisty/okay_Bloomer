2af559851d6cb71436d26d3adce1f0bf38754196
lufftewaffle <leeando90@gmail.com>
2024-05-17 10:30:02 +1200

    Fixed back_image issue, and added header

--- OB/okayBloomer.py
+++ OB/okayBloomer.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main_page():
     return render_template('main_page.html')
 
+# @app.route('/')
+# def water_journal:
+#     return()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
