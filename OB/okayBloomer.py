f1907f9a63b9c6a676f246e3448bb5e8e2860802
lufftewaffle <leeando90@gmail.com>
2024-05-16 22:27:16 +1200

    Baisc main page template

--- OB/okayBloomer.py
+++ OB/okayBloomer.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def login_page():
-    return "wassup"
+    return render_template('login_screen.html')
 
 
 if __name__ == "__main__":
