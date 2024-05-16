3b072fdbb65e87a444b05112bdb64d790c820595
lufftewaffle <leeando90@gmail.com>
2024-05-16 20:46:26 +1200

    baisc flask framework:

--- OB/okayBloomer.py
+++ OB/okayBloomer.py
@@ -1,1 +1,12 @@
-print("This is a line of text")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def login_page():
+    return "wassup"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
