1bcbca46aae3f01dcbeb4931caba1f84f738e5ee
lufftewaffle <leeando90@gmail.com>
2024-05-17 20:48:00 +1200

    :/

--- OB/okayBloomer.py
+++ OB/okayBloomer.py
@@ -7,13 +7,11 @@ app = Flask(__name__)
 def main_page():
     return render_template('main_page.html')
 
-# @app.route('/')
-# def water_journal:
-#     return()
+
+@app.route('/plant_profiles.html')
+def plant_profile():
+    return render_template('plant_profiles.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
