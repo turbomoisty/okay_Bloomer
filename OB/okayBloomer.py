cced4e8f970e46049ba20674c7e1085f0b78f12b
lufftewaffle <leeando90@gmail.com>
2024-05-20 11:30:23 +1200

    linked pages

--- OB/okayBloomer.py
+++ OB/okayBloomer.py
@@ -3,16 +3,19 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def main_page():
-#     return render_template('main_page.html')
-
-# @app.route('/login_page')
-# def main_page():
-#     return render_template('login_page.html')
-###########REMEMBER TO LINK THIS IN THE HTML FILE AT LINE 17: main_page.htnml########
-
 @app.route('/')
+def main_page():
+    return render_template('main_page.html')
+
+
+@app.route('/login_page')
+def login_page():
+    return render_template('login_page.html')
+
+
+##########REMEMBER TO LINK THIS IN THE HTML FILE AT LINE 17: main_page.html########
+
+@app.route('/about_us')
 def about_us():
     return render_template('about_us.html')
 
@@ -20,6 +23,9 @@ def about_us():
 @app.route('/plant_profiles')
 def plant_profiles():
     return render_template('plant_profiles.html')
+
+
+# just templates
 
 
 if __name__ == "__main__":
