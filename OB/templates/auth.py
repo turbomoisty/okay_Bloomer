44639b53ac511de6150c0aebfd7be7dff6266e36
lufftewaffle <leeando90@gmail.com>
2024-05-31 11:12:17 +1200

    added ERD diagram under images

--- OB/templates/auth.py
+++ OB/templates/auth.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, render_template
+
+from flask import *
+import request
+from models import bloomerUser
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login_page')
+@auth.route('/login_page', methods=['GET', 'POST'])
 def login_page():
+    message = 'default text'
+    if request.method == "POST" and 'userEmail' in request.form and 'userPassword' in request.form:
+        email = request.form['userEmail']
+        password = request.form['userPassword']
+        cu
     return render_template('login_page.html')
--- OB/templates/models.py
+++ OB/templates/models.py
@@ -13,12 +13,20 @@ class bloomerUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(10), db.foreignKey('user.id'), index=True, unique=True)
     userEmail = db.Column(db.String(20), index=True, unique=True)
-    usePassword = db.Column(db.String(10), index=False, uniqe=True)
+    userPassword = db.Column(db.String(10), index=False, uniqe=True)
 
 
-class plantPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    post_text = db.Column(db.String(100), index=False, unique=False)
+class journalEntry(db.Model):
+    entry_id = db.Column(db.Integer, primary_key=True)
+    journal_id = db.Column(db.Integer, db.foreignKey('journal_entry.id'), index=True, unique=False)
+    plant_journal_id = db.Column(db.Integer, db.ForeignKey('plant_journal_entry.id'), index=True, unique=False)
+    journal_entry = db.Column(db.string(200), index=False, unique=True)
+
+
+class communityPost(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    userPost_id = db.Column(db.integer, db.ForeignKey('post_creator.id'), index=True, unique=False)
+    postText_id = db.Column(db.integer, )
 
 
 # ###NOTES#### unique: When set to True, the values in the column has to be unique index: Wen True, the column is
@@ -26,7 +34,7 @@ class plantPost(db.Model):
 # journal_entries = db.relationship('Class name - Referring to the 'many' side',backref='attribute name to dynamically allocate',lazy='dynamic')
 # 'One' side of the relationship syntax
 
-#
+#    reviews = db.relationship('Review', backref = 'book', lazy = 'dynamic')
 # searchable by its values primary key: when True, the column acts as the primary key What is a primary key: Each
 # value is unique across the whole table, primary key cannot contain a null value and is automatically indexed by the
 # database
@@ -35,4 +43,3 @@ class userPlant(db.Model):
     plant_name = db.Column(db.String(21), index=True, unique=False)
     plant_species = db.Column(db.String(30), index=True, unique=False)
     user_notes = db.Column(db.String(100), index=False, Unique=False)
-
