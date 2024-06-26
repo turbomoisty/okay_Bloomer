511066f26763f34e0f49943f01d07e7409f9e8ac
AIRSA87 <abirawlins8@gmail.com>
2024-06-26 15:52:49 +1200

    edited pages

---
+++ OB/static/services_page.js
@@ -0,0 +1,12 @@
+// Function to add background color to nav on scroll
+  const navbar = document.querySelector('nav');
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+          navbar.classList.add('scrolled');
+      } else {
+          navbar.classList.remove('scrolled');
+      }
+  });
+});
+
+
